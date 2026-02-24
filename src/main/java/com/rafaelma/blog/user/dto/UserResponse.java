2d1467d177db72baeacb4a419b30fd60c263296c
eiri0 <rafaelmontoya@outlook.com>
2026-02-23 21:31:32 -0700

    feat: add user response class

---
+++ src/main/java/com/rafaelma/blog/user/dto/UserResponse.java
@@ -0,0 +1,30 @@
+package com.rafaelma.blog.user.dto;
+
+public class UserResponse {
+
+    private Long id;
+    private String userName;
+
+    public UserResponse() {}
+
+    public UserResponse(Long id, String userName){
+        this.id = id;
+        this.userName = userName;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+}
