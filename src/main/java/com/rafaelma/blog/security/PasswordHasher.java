d9d571b92aede6658461e710bc3166456f4f4e5a
rafael montoya <rafaelmontoya@outlook.com>
2026-05-27 18:33:15 -0700

    feat: change variables to constants in password hasher class

--- src/main/java/com/rafaelma/blog/security/PasswordHasher.java
+++ src/main/java/com/rafaelma/blog/security/PasswordHasher.java
@@ -4,8 +4,15 @@ import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 public class PasswordHasher {
 
+    private static final int SALT_LENGTH = 16;
+    private static final int HASH_LENGTH = 32;
+    private static final int PARALLELISM = 1;
+    private static final int MEMORY = 60000;
+    private static final int ITERATIONS = 10;
+    private static Argon2PasswordEncoder encoder ;
+    
     public static String hashPassword(String password) {
-         Argon2PasswordEncoder encoder = new Argon2PasswordEncoder(16, 32, 1, 60000, 10);
+        encoder = new Argon2PasswordEncoder(SALT_LENGTH, HASH_LENGTH, PARALLELISM, MEMORY, ITERATIONS);
         return encoder.encode(password);
     }
    
